d2b29acb1e2dab0873b4770314ec3b6df2a01a7a
Akhil Dasika <akhildasika@amdg.rockhursths.edu>
2020-03-11 08:50:51 -0500

    game

--- travellingFunction.py
+++ travellingFunction.py
@@ -1,16 +1,110 @@
 global player
+import random
 
+def die():
+    print("You died!!!")
+    print ("Game Over!")
+
+def win():
+    print("You win!!!")
+    print("Game Over!")
+
+def mazeDie():
+    print("You run into a giant troll")
+    die()
+
+def mazeend():
+    print("You have escaped!")
+    win()
     
+def mazewrong():
+    print("You reach a dead end")
+    print("You make your way back")
+    maze3()
+
+def maze5():
+    print("You continue in the maze")
+    a = int(input("1 for forward. 2 for left. 3 for right."))
+    if(a == 2):
+        mazeend()
+    else:
+        mazeDie()           
+def maze4():
+    print("You continue in the maze")
+    a = int(input("1 for forward. 2 for left. 3 for right."))
+    if(a == 2):
+        maze5()
+    else:
+        mazewrong()
+
+def maze3():
+    print("You continue in the maze")
+    a = int(input("1 for forward. 2 for left. 3 for right."))
+    if(a == 3):
+        maze4()
+    else:
+        mazewrong()        
+
+def maze2():
+    print("You continue in the maze")
+    a = int(input("1 for forward. 2 for left. 3 for right."))
+    if(a == 1):
+        maze3()
+    else:
+        mazewrong()
+            
+def light():
+    print("You enter the Light")
+    print("You enter a room. There is a man standing inside")
+    print("'To escape you must reach the end of the maze' he says")
+    print("You are at the start of a maze")
+    a = int(input("1 for forward. 2 for left. 3 for right."))
+    if(a == 1):
+        maze2()
+    else:
+        mazeDie()
+          
+def fight1():
+    a = random.randint(1,2)
+    if(a == 1):
+        print("You manage to overpower him")
+        print("'Don't enter the light' he says.")
+        light()
+    else:
+        print("He was too strong!")
+        die()
+
+def run1():
+    a = random.randint(1,5)
+    if(a <= 2):
+        print("You manage to keep away.You sneak around and go back to the light")
+        light()
+    else:
+        print("He was too fast!")
+        die()
+        
+        
+
 def back():
     print("You go home. The end.")
 
 def inside():
     print("You a staircase going up and one going down. Where do you go?")
-    a = input("1 for up. 2 for down.")
+    a = int(input("1 for up. 2 for down."))
     if(a == 1):
         up()
     if(a == 2):
         down()
+
+def outside():
+    print("You walk around to the back of the house and towards the woods behind the house.")
+    print("You walk in and see a light coming from the distance You go in")
+    print("You see a man standing gaurd. He lunges at you")
+    a = int(input("1 to fignt 2 to run"))
+    if(a == 1):
+        fight1()
+    if(a == 2):
+        run1()
     
 
 def start():
@@ -18,7 +112,7 @@ def start():
     print("1 to go inside")
     print("2 to walk around the house")
     print("3 to go away")
-    b = input("Which one?")
+    b = int(input("Which one? "))
     if(b == 1):
         inside()
     elif(b == 2):
@@ -26,9 +120,7 @@ def start():
     elif(b == 3):
         back()
 
-def intro():
-    player = input("What is your name?")
-    print(f"Hello {player}")
-    start()    
 
-intro()
+player = input("What is your name? ")
+print(f"Hello {player}")
+start()    
